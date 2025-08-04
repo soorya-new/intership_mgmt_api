f380af6cdb85d396488f6a2063d836047371f9a0
Suryananda <sooryasudeesh181@gmail.com>
2025-08-04 23:08:02 +0530

    model committed

--- internship/admin.py
+++ internship/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User,Internship,Enrollment,Task,TaskAssignment,TaskSubmission,MockInterview,ActivityLog
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Internship)
+admin.site.register(Enrollment)
+admin.site.register(Task)
+admin.site.register(TaskAssignment)
+admin.site.register(TaskSubmission)
+admin.site.register(MockInterview)
+admin.site.register(ActivityLog)
--- internship_mgmt_api/settings.py
+++ internship_mgmt_api/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
-    'internship'
+    'rest_framework',
+    'internship',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'internship.User'
